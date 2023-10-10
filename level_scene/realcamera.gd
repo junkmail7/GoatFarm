64914c034b36269e802da4736aac726e096d06da
William Pannell <55947686+junkmail7@users.noreply.github.com>
2023-10-10 17:56:01 -0500

    fix camera, make inputs less dumb

--- level_scene/realcamera.gd
+++ level_scene/realcamera.gd
@@ -1,21 +1,69 @@
 extends Camera2D
+
 var object1 = null
-var object2= null
+var object2 = null
 var object3 = null
+var initial_distance = 0.0 # Store the initial distance between object1 and object2
 
 func _ready():
 	object1 = get_node("../player")
 	object2 = get_node("../enemy")
 	object3 = get_node(".")
 	
-	print(object1)
+	initial_distance = calculate_initial_distance()
 
 func _process(delta):
-	object3.position = (object1.global_position + object2.position) * 0.5
-
-	var zoom_factor1 = abs(object1.position.x-object2.position.x)/(1920)
-	var zoom_factor2 = abs(object1.position.y-object2.position.y)/(1080)
-	var zoom_factor = max(max(zoom_factor1, zoom_factor2), 0.5)
-	#print(zoom_factor1)
+	var current_distance = 0.0
+	
+	if object2 != null:
+		current_distance = object1.global_position.distance_to(object2.global_position)
+	else:
+		current_distance = object1.global_position.distance_to(object3.global_position)
+	
+	var zoom_factor = initial_distance / current_distance
+	
+	# Limit the zoom factor within a specific range (e.g., 0.5 to 1.8)
+	zoom_factor = clamp(zoom_factor, 0.5, 1.8)
+	
 	self.zoom = Vector2(zoom_factor, zoom_factor)
-	#print(self.zoom)
+	
+	# Set the camera position to the midpoint between object1 and object2
+	if object2 != null:
+		object3.position = (object1.global_position + object2.global_position) / 2
+	else:
+		object3.position = (object1.global_position + object1.global_position) / 2
+
+func calculate_initial_distance() -> float:
+	if object2 != null:
+		return object1.global_position.distance_to(object2.global_position)
+	else:
+		return object1.global_position.distance_to(object3.global_position)
+
+
+
+#extends Camera2D
+#
+#var object1 = null
+#var object2 = null
+#var object3 = null
+#var initial_distance = 0.0 # Store the initial distance between object1 and object2
+#
+#func _ready():
+#	object1 = get_node("../player")
+#	object2 = get_node("../enemy")
+#	object3 = get_node(".")
+#
+#	initial_distance = object1.global_position.distance_to(object2.global_position)
+#
+#func _process(delta):
+#	var current_distance = object1.global_position.distance_to(object2.global_position)
+#	var zoom_factor = initial_distance / current_distance
+#
+#	# Limit the zoom factor within a specific range (e.g., 0.5 to 2.0)
+#	zoom_factor = clamp(zoom_factor, 0.5, 1.8)
+#
+#	self.zoom = Vector2(zoom_factor, zoom_factor)
+#
+#	# Set the camera position to the midpoint between object1 and object2
+#	object3.position = (object1.global_position + object2.global_position) / 2
+	
