aedead7bd92f6d8acf596a22109200eee29bc818
William Pannell <55947686+junkmail7@users.noreply.github.com>
2023-10-26 18:24:24 -0500

    music and stuff

--- blood/surface.gd
+++ blood/surface.gd
@@ -1,5 +1,7 @@
 extends CanvasItem
 
+@export var timer : Timer
+
 var blood_color : Color = Color(0.5, 0, 0, 1.0)
 var blood_thickness : float = 2.0
 var blood_trails : Array = []
@@ -7,7 +9,7 @@ var blood_trails : Array = []
 func draw_blood(draw_pos: Vector2, blood_length: float) -> void:
 	#var local_pos = global_transform.affine_inverse().xform(draw_pos)
 	var local_pos = to_local(draw_pos)
-	print(local_pos)
+	#print(local_pos)
 	var end_pos = local_pos + Vector2(blood_length, 0)  # This creates a horizontal line; adjust as needed
 	blood_trails.append({ "start": local_pos, "end": end_pos })  # Store the blood trail information
 	queue_redraw()  # Request a redraw
@@ -18,6 +20,7 @@ func draw_blood(draw_pos: Vector2, blood_length: float) -> void:
 func _draw() -> void:
 	for trail in blood_trails:
 		draw_line(trail.start, trail.end, blood_color, blood_thickness)
+		timer.start()
 #
 #func _process(delta):
 #	if Input.is_action_pressed("right_click"):
@@ -32,43 +35,7 @@ func to_local(p_global):
 	return get_global_transform().affine_inverse().basis_xform(p_global)
 	
 
-
-
-
-
-
-
-
-
-
-
-#class_name Paint
-#
-#var surface_image : Image = Image.create(1500, 1000, false, Image.FORMAT_RGBA8)
-#var surface_texture : ImageTexture = ImageTexture.new()
-#var blood_texture : ImageTexture = ImageTexture.new()
-#var blood_image : Image = Image.new()
-#
-#func _ready() -> void:
-#	# Fill the surface with a red color
-#	surface_image.fill(Color(1.0, 0, 0, 1.0))
-#	surface_texture.create_from_image(surface_image)
-#	texture = surface_texture
-#
-#	# Load the blood image and create a texture from it
-#	blood_image.load("res://gfx/blood1.png")
-#	assert(blood_image.get_width() > 0 && blood_image.get_height() > 0, "Blood image failed to load.")
-#	blood_texture.create_from_image(blood_image)
-#
-#func draw_blood(draw_pos : Vector2):
-#	print("Drawing blood at position:", draw_pos)
-#
-#	# Blit the blood image onto the surface at the specified position
-#	var source_rect = Rect2(Vector2(0, 0), Vector2(blood_image.get_width(), blood_image.get_height()))
-#	surface_image.blit_rect(blood_image, source_rect, draw_pos)
-#	surface_texture.create_from_image(surface_image)
-#	texture = surface_texture
-#
-#func _physics_process(delta: float) -> void:
-#	if(Input.is_action_pressed("right_click")):
-#		draw_blood(get_global_mouse_position())
+func _on_timer_timeout():
+	print("tima cleared")
+	blood_trails.clear()  # Clear the blood_trails array
+	queue_redraw()  
--- level_2_scene/level_2.gd
+++ level_2_scene/level_2.gd
@@ -5,7 +5,7 @@ var character_scene2 : PackedScene = preload("res://character_2/character_2.tscn
 var enemy : PackedScene = preload("res://enemy_scene/enemy.tscn")
 var character_2_enemy : PackedScene = preload("res://character_2_enemy/character_2_enemy.tscn")
 var enemy_ai : PackedScene = preload("res://enemy_ai/enemy_ai.tscn")
-
+var blood : PackedScene = preload("res://blood/blood.tscn")
 # Called when the node enters the scene tree for the first time.
 
 signal char_to_add(char : CharacterBody2D)
@@ -28,6 +28,30 @@ func _ready():
 			instantiate_character(character_scene2)
 		instantiate_character(enemy_ai)
 
+var object1 = null
+var object2 = null
+
+func _physics_process(delta: float) -> void:
+	if(Input.is_action_just_pressed("click")):
+		for i in range(10):
+			var blood_instance = blood.instantiate()
+			blood_instance.global_position = get_global_mouse_position()
+			add_child(blood_instance)
+			
+	if char_arr.size() == 2:	
+		object1 = char_arr[0]
+		object2 = char_arr[1]
+		if(object1.state_machine.current_state.name == "hurt"):
+			#print(object1.state_machine.current_state.name)
+			var blood_instance = blood.instantiate()
+			blood_instance.global_position = object1.global_position
+			add_child(blood_instance)
+		if(object2.character_state_machine.current_state.name == "hurt"):
+			#print(object1.state_machine.current_state.name)
+			var blood_instance = blood.instantiate()
+			blood_instance.global_position = object2.global_position
+			add_child(blood_instance)
+
 # Function to instantiate a character
 func instantiate_character(character_scene : PackedScene) -> void:
 	var character_instance = character_scene.instantiate()
@@ -37,3 +61,7 @@ func instantiate_character(character_scene : PackedScene) -> void:
 		add_child(character_instance)
 		emit_signal("char_to_add", character_instance)
 	
+var char_arr = []
+
+func _on_char_to_add(char):
+	char_arr.push_back(char)
